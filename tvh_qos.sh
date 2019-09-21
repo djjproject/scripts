3aef5e7ce6d41833a5d148a9aa9575e31dd122d7
djjproject <djj9404@gmail.com>
2019-09-21 20:48:58 +0900

    Update tvh_qos.sh

--- tvh_qos.sh
+++ tvh_qos.sh
@@ -28,3 +28,46 @@ while true; do
 done 
 
 exit 0
+#!/bin/bash
+
+if pidof -o %PPID -x "$0"; then
+   exit 1
+fi
+
+WONDER=/usr/sbin/wondershaper
+INTERFACE=enp2s0
+NETRATE=97280
+
+$WONDER clear $INTERFACE
+
+TMP_VIEWCNT=0
+while true; do
+	sleep 3
+	JSONDATA=`curl -s http://id:pw@ipaddr:9981/api/status/subscriptions`
+	VIEWCNT=`echo $JSONDATA | jq .totalCount`
+
+	DNRATE=0
+	UPRATE=0
+
+	if [ "$VIEWCNT" -ne "$TMP_VIEWCNT" ]; then
+		sleep 2
+
+		for i in `seq 0 $VIEWCNT`
+		do
+			DNNOW=`echo $JSONDATA | jq .entries[$i].in`
+			DNRATE=`echo $DNRATE + $DNNOW | bc`
+			UPNOW=`echo $JSONDATA | jq .entries[$i].out`
+			UPRATE=`echo $UPRATE + $UPNOW | bc`
+		done
+
+		DNRATE=`echo "$NETRATE-$DNRATE*8/1024" | bc`
+		UPRATE=`echo "$NETRATE-$UPRATE*8/1024" | bc`
+
+		$WONDER $INTERFACE $DNRATE $UPRATE
+		TMP_VIEWCNT=$VIEWCNT
+		echo "CURRNT COUNT : $VIEWCNT | CURRENT SPEED : $DNRATE / $UPRATE"
+	fi
+
+done 
+
+exit 0
