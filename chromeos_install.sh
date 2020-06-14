32b03fce349d6773e7d627b1f1c8f77950898b7d
djjproject <djj9404@gmail.com>
2020-06-14 23:10:23 +0900

    Update chromeos_install.sh

--- chromeos_install.sh
+++ chromeos_install.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install pv
 sudo apt-get install cgpt
 sudo fdisk -l
-sudo read -p "enter disk name (example : /dev/sda) : " DISK
+read -p "enter disk name (example : /dev/sda) : " DISK
 sudo bash chromeos-install.sh -src rammus_recovery.bin -dst $DISK
